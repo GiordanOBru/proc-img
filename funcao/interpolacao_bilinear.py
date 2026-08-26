ef86d3310a3edbccdf516fb32778423225a2eac5
giordano <giordano.santos@mail.uft.edu.br>
2026-08-26 10:37:37 -0300

    finalzacao funcional do interpolacao

--- funcao/interpolacao_bilinear.py
+++ funcao/interpolacao_bilinear.py
@@ -4,19 +4,19 @@ import numpy as np
 
 def reducao_bilinear(img):
     #define o tamanho da altura e largura
-    largura = img.shape[0]
-    altura = img.shape[1]
+    altura = img.shape[0]
+    largura = img.shape[1]
 
     #cria uma nova matriz para nova imagem com tamanho reduzido em 2 vezes
-    img_nova = np.zeros((int(largura/2), int(altura/2), img.shape[2]))
+    img_nova = np.zeros((int(altura/2), int(largura/2), img.shape[2]))
 
     #auxiliares para percorrer imagem nova
     aux_largura = 0
     aux_altura = 0 
 
-    for i in range(0, int(largura/2)):
+    for i in range(0, int(altura/2)):
         aux_largura = 0
-        for j in range(0, int(altura/2)):
+        for j in range(0, int(largura/2)):
             #copia o pixel da imagem original na nova imagem pegando os 4 ao redor e fazendo a média
             # (f(i,j) +f(i,j+1) + f(i+1,j) + f(i+1,j+1)) / 4
             media_reducao = (img[aux_altura][aux_largura]+img[aux_altura][aux_largura+1]+img[aux_altura+1][aux_largura]+img[aux_altura+1][aux_largura+1])/4
@@ -29,41 +29,41 @@ def reducao_bilinear(img):
 
 def ampliacao_bilinear(img):
     #define o tamanho da altura e largura
-    largura = img.shape[0]
-    altura = img.shape[1]
+    altura = img.shape[0]
+    largura = img.shape[1]
 
     #amplia o tamanho da matriz da nova imagem em 2 vezes
     n_largura = int(largura*2)
     n_altura = int(altura*2)
 
     #cria nova matriz vazia do tamanho dobrado da imagem anterior
-    img_nova = np.zeros((n_largura, n_altura, img.shape[2]))
+    img_nova = np.zeros((n_altura, n_largura, img.shape[2]))
 
     #auxiliar para percorrer imagem
-    aux_largura = 0
     aux_altura = 0
+    aux_largura = 0
 
-    for i in range(0, n_largura, 2):
+    for i in range(0, n_altura, 2):
         aux_largura = 0
-        for j in range(0, n_altura, 2):
+        for j in range(0, n_largura, 2):
             #insere valores originais na nova matriz
             img_nova[i][j] = img[aux_altura][aux_largura]
-            aux_largura += 2
-        aux_altura += 2
+            aux_largura += 1
+        aux_altura += 1
 
     #preenche os espaços com calculo médio dos vizinhos mais próximo
-    for i in range(0, n_largura - 1, 2):
-        for j in range(0, n_altura - 1, 2):
+    for i in range(0, n_altura - 2, 2):
+        for j in range(0, n_largura - 2, 2):
             #a = (f(i,j) + f(i,j+1)) / 2
-            img_nova[i][j+1] = (img_nova[i][j]+img_nova[i][i+2])/2
+            img_nova[i][j+1] = (img_nova[i][j] + img_nova[i][j+2])/2
             #b = (f(i,j) + f(i+1,j)) / 2
-            img_nova[i+1][j] = (img_nova[i][j]+img_nova[i+2][j])/2
+            img_nova[i+1][j] = (img_nova[i][j] + img_nova[i+2][j])/2
             #c = (f(i,j) + f(i,j+1) + f(i+1,j) + f(i+1,j+1)) / 4 
-            img_nova[i+1][j+1] = (img_nova[i][j]+img_nova[i][j+2]+img_nova[i+2][j]+img_nova[i+2][j+2])/4
+            img_nova[i+1][j+1] = (img_nova[i][j] + img_nova[i][j+2] + img_nova[i+2][j] + img_nova[i+2][j+2])/4
             #d = (f(i,j+1) + f(i+1,j+1)) / 2
-            img_nova[i+1][j+2] = (img_nova[i][j+2]+img_nova[i+2][j+2])/2
+            img_nova[i+1][j+2] = (img_nova[i][j+2] + img_nova[i+2][j+2])/2
             #e = (f(i+1,j) + f(i+1,j+1)) / 2  
-            img_nova[i+1][j+2] = (img_nova[i+2][j]+img_nova[i+2][j+2])/2
+            img_nova[i+2][j+1] = (img_nova[i+2][j] + img_nova[i+2][j+2])/2
 
     return img_nova
 
