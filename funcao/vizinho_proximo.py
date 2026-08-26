51332b9b86aca70cd33e757c92ebee8ecfa8cb2f
giordano <giordano.santos@mail.uft.edu.br>
2026-08-26 10:22:29 -0300

    ajuste de campos que estavam errados no local

--- funcao/vizinho_proximo.py
+++ funcao/vizinho_proximo.py
@@ -4,20 +4,20 @@ import numpy as np
 
 def reducao_vizinho(img):
     #define o tamanho da altura e largura
-    largura = img.shape[0]
-    altura = img.shape[1]
+    altura = img.shape[0]
+    largura = img.shape[1]
 
     #cria uma nova matriz para nova imagem com tamanho reduzido em 2 vezes
-    img_nova = np.zeros((int(largura/2), int(altura/2), img.shape[2]))
+    img_nova = np.zeros((int(altura/2), int(largura/2), img.shape[2]))
 
     #auxiliares para percorrer imagem nova
     aux_largura = 0
     aux_altura = 0 
 
     
-    for i in range(0, int(largura/2)):
+    for i in range(0, int(altura/2)):
         aux_largura = 0
-        for j in range(0, int(altura/2)):
+        for j in range(0, int(largura/2)):
             #copia o pixel da imagem original na nova imagem,
             img_nova[i][j] = img[aux_altura][aux_largura]
             #ajusta para pegar 2 casas após para reduzir de acordo
@@ -28,31 +28,31 @@ def reducao_vizinho(img):
 
 def ampliacao_vizinho(img):
     #define o tamanho da altura e largura
-    largura = img.shape[0]
-    altura = img.shape[1]
+    altura = img.shape[0]
+    largura = img.shape[1]
 
     #amplia o tamanho da matriz da nova imagem em 2 vezes
     n_largura = int(largura*2)
     n_altura = int(altura*2)
 
     #cria nova matriz vazia do tamanho dobrado da imagem anterior
-    img_nova = np.zeros((n_largura, n_altura, img.shape[2]))
+    img_nova = np.zeros((n_altura, n_largura, img.shape[2]))
 
     #auxiliar para percorrer imagem
     aux_largura = 0
     aux_altura = 0
 
-    for i in range(0, n_largura, 2):
+    for i in range(0, n_altura, 2):
         aux_largura = 0
-        for j in range(0, n_altura, 2):
+        for j in range(0, n_largura, 2):
             #insere valores originais na nova matriz
             img_nova[i][j] = img[aux_altura][aux_largura]
             aux_largura += 1
         aux_altura += 1
 
     #preenche os espaços com o vizinho mais próximo
-    for i in range(0, n_largura - 1, 2):
-        for j in range(0, n_altura - 1, 2):
+    for i in range(0, n_altura - 1, 2):
+        for j in range(0, n_largura - 1, 2):
             #repete o pixel em 4 posições
             img_nova[i][j+1] = img_nova[i][j]
             img_nova[i+1][j] = img_nova[i][j]
