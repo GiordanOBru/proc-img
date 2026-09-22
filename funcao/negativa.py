c63c56e59d31610aa9a914e92a21565f16a8f97e
montreal_gsantos <giordano.santos@montreal.com.br>
2026-09-22 11:38:15 -0300

    enviando funcao de negaçao imagem

---
+++ funcao/negativa.py
@@ -0,0 +1,33 @@
+import cv2
+
+import numpy as np
+
+def negativa(img):
+    #define o tamanho da imagem
+    altura = img.shape[0]
+    largura = img.shape[1]
+
+    #matriz nova para negativa
+    img_negativa = np.zeros((altura, largura), dtype=np.uint8)
+
+    #Soma simples entre pixels 
+    for i in range(altura - 1):
+        for j in range(largura - 1):
+            #analisa pixel e transforma em negativo
+            pixel_positivo = img[i,j]
+            pixel_negativo = 255 - pixel_positivo
+
+            img_negativa [i,j] = pixel_negativo
+
+    return img_negativa
+
+def main():
+    #lê imagem e deixa em tons de cinza
+    imgB = cv2.imread('imagens\imgB.png', cv2.IMREAD_GRAYSCALE)
+    img_soma = negativa(imgB)
+
+    #gera nova imagem negativa
+    cv2.imwrite('./imagens/negativa.png', img_soma)
+
+if __name__ == '__main__':
+    main()
